245799749f416fc03c96853c875b3225a27e8ded
kwonbi <kwonbi@pumpkinev.co.kr>
2023-11-02 17:45:34 +0900

    1102

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Header } from './src/components/Header/Header';
-import { HeaderWithoutCompound } from './src/components/HeaderWithoutCompound';
-import { HeaderGroup } from './src/components/Header/HeaderGroup';
+import { HookTestComponet } from './src/components/HookTestComponent';
+import { useCallback, useState } from 'react';
+import { Typography } from './src/components/Typography';
+import { Button } from './src/components/Button';
 
 export default function App() {
+
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const doSum = useCallback(() =>{
+    return ( a + b )
+  }, [a, b])
+
   return (
     <SafeAreaProvider>
-      <View style={{flex:1, }}>
-        {/* <HeaderWithoutCompound title ='HEDER' /> */}
+      <View style={{flex:1 }}>
         <Header>
-          <HeaderGroup>
-            <Header.Icon iconName='arrow-back'></Header.Icon>
-            <Header.Title title='HEADER'></Header.Title>
-          </HeaderGroup>
-
-          <Header.Icon iconName='close'></Header.Icon>
+          <Header.Title title='HEADER' ></Header.Title>
         </Header>
+
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+          <HookTestComponet a={a} b={b}></HookTestComponet>
+
+          <Typography>현재 callback 으로 계산된 값 : {doSum()}</Typography>
+
+          <Button onPress={()=>{
+            console.log('press'),
+            setA(a+1)
+          }}>
+            <Typography>A 더하기</Typography>
+          </Button>
+        </View>
+
       </View>
     </SafeAreaProvider>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,66 @@
 import React from 'react';
-import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
-import { View, Dimensions } from 'react-native';
+import { SafeAreaInsetsContext, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View, Dimensions, useWindowDimensions } from 'react-native';
 import { Spacer } from '../Spacer';
 import { HeaderTitle } from './HeaderTitle';
 import { HeaderIcon } from './HeaderButton';
 import { HeaderGroup } from './HeaderGroup';
 
-const { width } = Dimensions.get('window');
+// const { width } = Dimensions.get('window');
 
-export class Header extends React.Component{
-    render(){
-        return(
-            <SafeAreaInsetsContext.Consumer>
-                {insets=>(
-                    <View style={{paddingTop:insets.top,}}>
-                        <View 
-                            style={{
-                                width:width, 
-                                flexDirection:'row', 
-                                height:56, 
-                                borderBottomColor:'gray', 
-                                borderBottomWidth:1, 
-                                alignItems:'center'
-                            }}>
+export const Header= (props)=>{
+    const insets = useSafeAreaInsets();
+    const {width} = useWindowDimensions();
 
-                            <Spacer horizontal={true} space={12}/>
-                            <View style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
-                                {this.props.children}
-                            </View>
-                            <Spacer horizontal={true} space={12}/>
-                        </View>
-                    </View>
-                )}
-            </SafeAreaInsetsContext.Consumer>
-        )
-    }
+    return (
+        <View style={{paddingTop:insets.top}}>
+            <View style={{
+                width:width, 
+                flexDirection:'row', 
+                height:56,
+                borderBottomColor:'gray',
+                borderBottomWidth:1,
+                alignItems:'center'
+            }}>
+               <Spacer horizontal={true} space={12}/>
+                <View style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
+                    {props.children}
+                </View>
+                <Spacer horizontal={true} space={12}/>
+            </View>
+        </View>
+    )
 }
+
+// export class Header extends React.Component{
+//     render(){
+//         return(
+//             <SafeAreaInsetsContext.Consumer>
+//                 {insets=>(
+//                     <View style={{paddingTop:insets.top,}}>
+//                         <View 
+//                             style={{
+//                                 width:width, 
+//                                 flexDirection:'row', 
+//                                 height:56, 
+//                                 borderBottomColor:'gray', 
+//                                 borderBottomWidth:1, 
+//                                 alignItems:'center'
+//                             }}>
+
+//                             <Spacer horizontal={true} space={12}/>
+//                             <View style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
+//                                 {this.props.children}
+//                             </View>
+//                             <Spacer horizontal={true} space={12}/>
+//                         </View>
+//                     </View>
+//                 )}
+//             </SafeAreaInsetsContext.Consumer>
+//         )
+//     }
+// }
 
 Header.Title = HeaderTitle;
 Header.Icon = HeaderIcon;
-
-
-
-
-
 Header.Group = HeaderGroup;
