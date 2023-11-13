18f7c39318356a0047c44f2aaaf6ee8811b3b598
kwonbi <kwonbi@pumpkinev.co.kr>
2023-11-13 14:54:24 +0900

    1113

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Header } from './src/components/Header/Header';
-import { HookTestComponet } from './src/components/HookTestComponent';
-import { useCallback, useState } from 'react';
-import { Typography } from './src/components/Typography';
-import { Button } from './src/components/Button';
 
 export default function App() {
 
