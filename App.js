f7e18232c39573bc2d6d694674cc3516acfe1635
kwonbi <kwonbi@pumpkinev.co.kr>
2023-11-13 14:52:45 +0900

    완료

--- App.js
+++ App.js
@@ -8,34 +8,9 @@ import { Button } from './src/components/Button';
 
 export default function App() {
 
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
-
-  const doSum = useCallback(() =>{
-    return ( a + b )
-  }, [a, b])
-
   return (
     <SafeAreaProvider>
-      <View style={{flex:1 }}>
-        <Header>
-          <Header.Title title='HEADER' ></Header.Title>
-        </Header>
 
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-          <HookTestComponet a={a} b={b}></HookTestComponet>
-
-          <Typography>현재 callback 으로 계산된 값 : {doSum()}</Typography>
-
-          <Button onPress={()=>{
-            console.log('press'),
-            setA(a+1)
-          }}>
-            <Typography>A 더하기</Typography>
-          </Button>
-        </View>
-
-      </View>
     </SafeAreaProvider>
   );
 }
